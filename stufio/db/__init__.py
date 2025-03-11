0384588d002f31f098061574640fa5f24e99a322
IF <i.finchuk@gmail.com>
2025-03-11 16:35:11 +0100

    fix DB

---
+++ stufio/db/__init__.py
@@ -0,0 +1,14 @@
+"""
+Database connections and utilities for Stufio framework.
+"""
+
+# Import the modules to make them available directly from stufio.db
+from . import clickhouse
+from . import mongo
+
+# You can also import specific classes to expose at the package level
+# For example:
+# from .clickhouse import ClickHouseClient
+# from .mongo import MongoDatabase, get_database
+
+__all__ = ["clickhouse", "mongo"]
