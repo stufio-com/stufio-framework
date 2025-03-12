e67bedb634e6059d81aef4907d78b29baa8e8fb9
IF <i.finchuk@gmail.com>
2025-03-12 18:22:37 +0100

    Tokem model - fix relation

--- stufio/models/token.py
+++ stufio/models/token.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 from typing import Optional
-from odmantic import Field
+from odmantic import Field, Reference
 from stufio.db.mongo_base import MongoBase, datetime_now_sec
+from stufio.models import User
 from datetime import datetime, timedelta
 from stufio.core.config import get_settings
-from bson import ObjectId
+
 
 settings = get_settings()
 
@@ -18,6 +19,6 @@ def datetime_expires_sec() -> datetime:
 
 class Token(MongoBase):
     token: str
-    authenticates_id: Optional[ObjectId] = None  # Store just the ObjectId instead of a reference
+    authenticates_id: User = Reference()  # Store just the ObjectId instead of a reference
     created: datetime = Field(default_factory=datetime_now_sec)
     expires: datetime = Field(default_factory=datetime_expires_sec)
